c9f2679f2427a5afd1bce2a843eb4a1d8dbd83af
Bart Kalanski <bkalanski@gmail.com>
2020-06-11 13:43:53 +0100

    Added welcome page and option to pick flags from a chosen continent

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-//Fetch
+// fetch
+
 class Fetch {
-    async getCurrent() {
+    async getCurrent(input) {
         const response = await fetch(
-            `https://restcountries.eu/rest/v2/region/europe`
+            `https://restcountries.eu/rest/v2/region/${input}/`
         );
         const data = await response.json();
-
-        console.log(data);
-
         return data;
+
     }
 }
-let randomNumber = Math.floor(Math.random() * 54);
-let randomNumberOne = Math.floor(Math.random() * 54);
-let randomNumberTwo = Math.floor(Math.random() * 54);
-let randomNumberThree = Math.floor(Math.random() * 54);
-let randomNumberFour = Math.floor(Math.random() * 54);
-let randomNumberFive = Math.floor(Math.random() * 54);
-//UI
+
+// ui
+
 class UI {
     constructor() {
         this.oneContainer = document.getElementById("one")
@@ -32,73 +27,104 @@ class UI {
         this.tenContainer = document.getElementById("ten")
         this.elevenContainer = document.getElementById("eleven")
         this.twelveContainer = document.getElementById("twelve");
-        this.country;
     }
 
     populateUI(data) {
+
+        // number of countries in a specified region
+
+        let numberOfCountries = data.length;
+
+        // random number generator
+
+        let randomNumber = Math.floor(Math.random() * numberOfCountries);
+        let randomNumberOne = Math.floor(Math.random() * numberOfCountries);
+        let randomNumberTwo = Math.floor(Math.random() * numberOfCountries);
+        let randomNumberThree = Math.floor(Math.random() * numberOfCountries);
+        let randomNumberFour = Math.floor(Math.random() * numberOfCountries);
+        let randomNumberFive = Math.floor(Math.random() * numberOfCountries);
+
+        // hide welcome page before game begins
+
+        document.getElementById("index-banner").style.display =
+            "none";
+
+        // populate cards with flags 
+
         this.oneContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumber].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.sevenContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumber].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.twoContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberOne].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.eightContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberOne].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.threeContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberTwo].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.nineContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberTwo].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.fourContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberThree].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.tenContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberThree].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.fiveContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberFour].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.elevenContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberFour].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.sixContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberFive].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
         this.twelveContainer.innerHTML = `
             <img class="front-face" src="${data[randomNumberFive].flag}" alt="flag" />
-            <img class="back-face" src="img/globe.png" alt="JS Badge" />
+            <img class="back-face" src="img/globe.png" alt="world-flag" />
             `;
     }
 }
-//Script
+// script
+
 const ft = new Fetch();
 const ui = new UI();
-document.body.addEventListener('click', () => {
-    ft.getCurrent().then((data) => {
+
+// add event listeners 
+const search = document.getElementById("getInputData")
+const button = document.getElementById("begin");
+
+// submit button
+
+button.addEventListener('click', () => {
+    const currentVal = search.value;
+
+    ft.getCurrent(currentVal).then((data) => {
+
         //call a UI method
         ui.populateUI(data);
     });
 });
 
-//Script
+// gameplay 
+
 const cards = document.querySelectorAll('.memory-card');
-console.log(cards)
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard;
@@ -134,7 +160,7 @@ function checkForMatch() {
 }
 
 function disableCards() {
-    //its a match!
+    // its a match!
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 
@@ -143,7 +169,7 @@ function disableCards() {
 
 function unflipCards() {
     lockBoard = true;
-    //not a match
+    // not a match
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
@@ -163,5 +189,5 @@ function resetBoard() {
 
     })
 })();
-//listens for click on specific card
+// listens for click on specific card
 cards.forEach(card => card.addEventListener('click', flipCard));
